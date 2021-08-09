ce069c33dc2b0a45c55e33231500c8b0d7390343
okasaka <okasaka@rightcode.co.jp>
2021-08-09 21:05:14 +0900

    add dashboard template

--- src/App.svelte
+++ src/App.svelte
@@ -1,67 +1,52 @@
 <script lang="ts">
-  import logo from "./assets/svelte.png";
-  import Counter from "./lib/Counter.svelte";
   import Balometer from "./components/Balometer.svelte";
+  import Chart from "./components/Chart.svelte";
 </script>
 
 <main>
-  <Balometer title="title" percentage={32} number={1} />
-  <img src={logo} alt="Svelte Logo" />
-  <h1>Hello Typescript!</h1>
-
-  <Counter />
-
-  <p>
-    Visit <a href="https://svelte.dev">svelte.dev</a> to learn how to build Svelte
-    apps.
-  </p>
-
-  <p>
-    Check out <a href="https://github.com/sveltejs/kit#readme">SvelteKit</a> for
-    the officially supported framework, also powered by Vite!
-  </p>
+  <div>
+    <h1>Dashboard</h1>
+    <section class="balometers">
+      <Balometer title="title" percentage={32} number={1} />
+      <Balometer title="title" percentage={32} number={1} />
+      <Balometer title="title" percentage={32} number={1} />
+    </section>
+    <section class="chart-area">
+      <Chart />
+    </section>
+  </div>
 </main>
 
 <style>
+  :global(html) {
+    background-color: #7f5f00;
+  }
+
+  :global(body) {
+    margin: 0;
+    padding: 0;
+  }
+
   :root {
     font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, Oxygen,
       Ubuntu, Cantarell, "Open Sans", "Helvetica Neue", sans-serif;
   }
 
   main {
-    text-align: center;
-    padding: 1em;
+    width: fit-content;
+    padding: 12px 36px;
     margin: 0 auto;
   }
 
-  img {
-    height: 16rem;
-    width: 16rem;
-  }
-
   h1 {
-    color: #ff3e00;
-    text-transform: uppercase;
-    font-size: 4rem;
-    font-weight: 100;
-    line-height: 1.1;
-    margin: 2rem auto;
-    max-width: 14rem;
+    color: white;
+  }
+  .balometers {
+    display: flex;
+    gap: 12px;
   }
 
-  /* p {
-    max-width: 14rem;
-    margin: 1rem auto;
-    line-height: 1.35;
-  } */
-
-  /* @media (min-width: 480px) {
-    h1 {
-      max-width: none;
-    }
-
-    p {
-      max-width: none;
-    }
-  } */
+  .chart-area {
+    margin-top: 12px;
+  }
 </style>
---
+++ src/components/Chart.svelte
@@ -0,0 +1,88 @@
+<div class="chart-container">
+  <!-- <table border="1"> -->
+  <table>
+    <tr><td /></tr>
+    <tr><td /></tr>
+    <tr><td /></tr>
+    <tr><td /></tr>
+    <tr><td /></tr>
+    <tr><td /></tr>
+    <tr><td /></tr>
+    <tr><td /></tr>
+    <tr><td /></tr>
+    <tr><td /></tr>
+    <tr><td /></tr>
+  </table>
+</div>
+
+<style>
+  .chart-container {
+    width: fit-content;
+    padding: 12px 24px 36px 36px;
+    background-color: teal;
+    border-radius: 8px;
+  }
+
+  table {
+    width: 640px;
+    margin: 12px 24px;
+    border-collapse: collapse;
+  }
+
+  tr {
+    position: relative;
+    height: 28px;
+    border-bottom: 1px solid rgba(255, 255, 255, 0.466);
+  }
+
+  tr:first-child::before,
+  tr:nth-child(2)::before,
+  tr:nth-child(3)::before,
+  tr:nth-child(4)::before,
+  tr:nth-child(5)::before,
+  tr:nth-child(6)::before,
+  tr:nth-child(7)::before,
+  tr:nth-child(8)::before,
+  tr:nth-child(9)::before,
+  tr:nth-child(10)::before,
+  tr:nth-child(11)::before {
+    content: "100%";
+    position: absolute;
+    top: 21px;
+    right: calc(100%);
+    color: white;
+    font-size: 8px;
+    padding-right: 4px;
+  }
+
+  tr:nth-child(2)::before {
+    content: "90%";
+  }
+  tr:nth-child(3)::before {
+    content: "80%";
+  }
+  tr:nth-child(4)::before {
+    content: "70%";
+  }
+  tr:nth-child(5)::before {
+    content: "60%";
+  }
+  tr:nth-child(6)::before {
+    content: "50%";
+  }
+  tr:nth-child(7)::before {
+    content: "40%";
+  }
+  tr:nth-child(8)::before {
+    content: "30%";
+  }
+  tr:nth-child(9)::before {
+    content: "20%";
+  }
+  tr:nth-child(10)::before {
+    content: "10%";
+  }
+  tr:nth-child(11)::before {
+    content: "0%";
+  }
+</style>
