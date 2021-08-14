bb243ef15eaf593cb8f5a76b5bc15324b2c510b7
okasaka <okasaka@rightcode.co.jp>
2021-08-14 16:36:27 +0900

    connect barometer data with json file

--- src/App.svelte
+++ src/App.svelte
@@ -1,33 +1,48 @@
 <script lang="ts">
   import Barometer from "./components/Barometer.svelte";
   import Chart from "./components/Chart.svelte";
+
+  let promise = getBarometerData();
+  async function getBarometerData() {
+    const res = await fetch("/api/barometer_data.json");
+    const data = await res.json();
+    if (res.ok) {
+      return data;
+    } else {
+      throw new Error();
+    }
+  }
 </script>
 
 <main>
   <div>
     <h1>Dashboard</h1>
     <section class="barometers">
-      <Barometer
-        barometer_color="chocolate"
-        canvas_id="canvas_3"
-        title="title"
-        percentage={32}
-        number={1}
-      />
-      <Barometer
-        barometer_color="orange"
-        canvas_id="canvas_2"
-        title="title"
-        percentage={50}
-        number={1}
-      />
-      <Barometer
-        barometer_color="gold"
-        canvas_id="canvas_1"
-        title="title"
-        percentage={32}
-        number={1}
-      />
+      {#await promise}
+        <p>loading...</p>
+      {:then res}
+        <Barometer
+          barometer_color="chocolate"
+          canvas_id="canvas_3"
+          title="20s"
+          percentage={res.barometer.twenties.percentage}
+          number={res.barometer.twenties.number}
+        />
+        <Barometer
+          barometer_color="orange"
+          canvas_id="canvas_2"
+          title="PASSED"
+          percentage={res.barometer.passed.percentage}
+          number={res.barometer.passed.number}
+        />
+        <Barometer
+          barometer_color="gold"
+          canvas_id="canvas_1"
+          title="AGREEMENT"
+          percentage={res.barometer.agreement.percentage}
+          number={res.barometer.agreement.number}
+        />
+      {/await}
     </section>
     <section class="chart-area">
       <Chart />
